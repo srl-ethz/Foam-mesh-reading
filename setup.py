8a2c43b406cbf687f4805c7ee66fbc9e7764da35
rzhangbq <rzhangbq@ust.hk>
2023-08-22 15:29:25 +0000

    update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,24 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='PyFoam2SDF',
-    version='0.1',
+    name="pyfoam2sdf",
+    version="0.1",
+    description="A Python package for converting foamMesh data to Signed Distance Fields (SDF).",
+    author="ZHANG Rushan",
+    author_email="rzhangbq@gmail.com",
+    url="https://github.com/srl-ethz/Foam-mesh-reading",
     packages=find_packages(),
+    install_requires=[
+        "numpy"
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
 )
